de6608c22dfe3a84d6201e1c222e1f91d00f4b04
ankityyyy <ankitdubey9431969639@gmail.com>
2025-11-09 16:03:02 +0530

    add

---
+++ Sum.test.js
@@ -0,0 +1,5 @@
+import { Sum } from "./Sum.js";
+
+test("add 2+2 is equal to 4", () => {
+  expect(Sum(2, 2)).toBe(4);
+});
---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app=express();
+import { Sum } from "./Sum.js";
+
+let port=5000;
+
+app.get("/test",(req,res)=>{
+     res.json({message:"i am root user"});
+})
+
+app.get("/getSum/:a/:b",async(req,res)=>{
+     const {a,b}=req.params;
+     res.json({
+          ans:Sum(parseInt(a),parseInt(b))
+     }) 
+})
+
+app.listen(port,()=>{
+     console.log(`app is listen on ${port} `)
+})
